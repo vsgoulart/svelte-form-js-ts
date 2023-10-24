e78f44928a13d85cfe5d0e6b65813e92e468d8bb
Vinicius Goulart <vinicius.goulart@camunda.com>
2023-10-24 15:46:19 +0100

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script>
+	import { onMount } from 'svelte';
+	import schema from './schema.json';
+	import '@bpmn-io/form-js/dist/assets/form-js.css';
+
+	onMount(async () => {
+		const { Form } = await import('@bpmn-io/form-js');
+		const form = new Form({
+			container: '#container'
+		});
+
+		form.importSchema(schema);
+	});
+</script>
+
+<main>
+	<div id="container" />
+</main>
+
+<style>
+	main {
+		width: 100vw;
+		height: 100vh;
+	}
+	#container {
+		width: 100%;
+		height: 100%;
+	}
+</style>
